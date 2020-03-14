04c55dcf95f90d4ef44e1c83f94e701958fd1b86
Daniel Stahl <danstahl1138@gmail.com>
2020-03-14 16:16:55 -0500

    fixed depednecies

--- src/writeYml.js
+++ src/writeYml.js
@@ -1,18 +1,18 @@
 const resolve = require('json-refs').resolveRefs
-const YAML = require('js-yaml')
+const YAML = require('yaml')
 const fs = require('fs')
 
-const root = YAML.load(fs.readFileSync('./docs/openapi.yml').toString());
+const root = YAML.parse(fs.readFileSync('./docs/openapi.yml').toString());
 const options = {
-    processContent:  (content)=>{
-        return YAML.load(content)
+    processContent: (content) => {
+        return YAML.parse(content)
     },
     filter: ['relative'],
-    location:'./docs/openapi.yml'
+    location: './docs/openapi.yml'
 }
-module.exports=()=>resolve(root, options).then( (results)=>{
+module.exports = () => resolve(root, options).then((results) => {
     fs.writeFileSync(
-        './docs/openapi_merged.yml', 
-        YAML.dump(results.resolved)
+        './docs/openapi_merged.yml',
+        YAML.stringify(results.resolved)
     )
 })
