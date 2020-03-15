aafa73e649e0493c67646a2a3c8df6c01ea77452
Daniel Stahl <danstahl1138@gmail.com>
2020-03-15 08:44:23 -0500

    updated schema parser

--- src/writeYml.js
+++ src/writeYml.js
@@ -1,18 +1,13 @@
-const resolve = require('json-refs').resolveRefs
+const refparser = require("@apidevtools/json-schema-ref-parser")
 const YAML = require('yaml')
 const fs = require('fs')
 
-const root = YAML.parse(fs.readFileSync('./docs/openapi.yml').toString());
-const options = {
-    processContent: (content) => {
-        return YAML.parse(content)
-    },
-    filter: ['relative'],
-    location: './docs/openapi.yml'
-}
-module.exports = () => resolve(root, options).then((results) => {
-    fs.writeFileSync(
-        './docs/openapi_merged.yml',
-        YAML.stringify(results.resolved)
-    )
+module.exports = () => Promise.resolve(() => {
+    const root = YAML.parse(fs.readFileSync('./docs/openapi.yml').toString())
+    refparser.dereference(root).then((parsed) => {
+        fs.writeFileSync(
+            './docs/openapi_merged.yml',
+            YAML.stringify(parsed)
+        )
+    })
 })
